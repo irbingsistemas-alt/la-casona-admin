589f2952b62f2ec9c479f4a0c47395a2f4a790fb
irbingsistemas-alt <irbing.sistemas@gmail.com>
2025-10-16 13:15:21 -0400

    Update script-login.js

--- script-login.js
+++ script-login.js
@@ -1,1 +1,11 @@
+function login() {
+  const usuario = document.getElementById("usuario").value;
+  const clave = document.getElementById("clave").value;
 
+  if (usuario === "admin" && clave === "casona17") {
+    localStorage.setItem("autenticado", "true");
+    window.location.href = "admin.html";
+  } else {
+    alert("Usuario o contraseña incorrectos");
+  }
+}
