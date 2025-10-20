329fece538afd9b1fab6e7393516c815c4c7060f
irbingsistemas-alt <irbing.sistemas@gmail.com>
2025-10-20 15:40:30 -0400

    Update script-login.js

--- script-login.js
+++ script-login.js
@@ -1,10 +1,8 @@
-// ✅ Configuración de Supabase
 const SUPABASE_URL = "https://ihswokmnhwaitzwjzvmy.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imloc3dva21uaHdhaXR6d2p6dm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NjU2OTcsImV4cCI6MjA3NjM0MTY5N30.TY4BdOYdzrmUGoprbFmbl4HVntaIGJyRMOxkcZPdlWU";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-// ✅ Función principal de login
 async function login() {
   const usuario = document.getElementById("usuario").value.trim();
   const clave = document.getElementById("clave").value.trim();
