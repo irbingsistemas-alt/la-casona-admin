d1929e7754dffcaefdaf28ef9992b7cec2fb88ba
Irbing A. Brizuela Sánchez <irbing.sistemas@gmail.com>
2025-10-24 11:30:54 -0400

    Update script-dependientes.js

--- scripts/script-dependientes.js
+++ scripts/script-dependientes.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 const supabase = createClient(
-  "https://your-project-url.supabase.co",
-  "your-public-anon-key"
+  "https://ihswokmnhwaitzwjzvmy.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imloc3dva21uaHdhaXR6d2p6dm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NjU2OTcsImV4cCI6MjA3NjM0MTY5N30.TY4BdOYdzrmUGoprbFmbl4HVntaIGJyRMOxkcZPdlWU"
 );
 
 let menu = [];
