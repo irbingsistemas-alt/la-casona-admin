fd58b5a217bbbf2f82f587086ac8c68131832b9c
Irbing A. Brizuela Sánchez <irbing.sistemas@gmail.com>
2025-10-25 15:02:50 -0400

    Update script-dependientes.js

--- scripts/script-dependientes.js
+++ scripts/script-dependientes.js
@@ -1,5 +1,8 @@
-// === Supabase ===
-const supabase = window.supabase;
+// === Inicializar Supabase ===
+const supabase = supabase.createClient(
+  'https://https://ihswokmnhwaitzwjzvmy.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imloc3dva21uaHdhaXR6d2p6dm15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA3NjU2OTcsImV4cCI6MjA3NjM0MTY5N30.TY4BdOYdzrmUGoprbFmbl4HVntaIGJyRMOxkcZPdlWU'
+
 
 // === Login ===
 document.getElementById('btn-login').addEventListener('click', async () => {
